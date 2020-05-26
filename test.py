500372ea1819dd450f075063fc8b9f9820ef6eef
Bharadwaj-Dontha <bharadwajdontha00@gmail.com>
2020-05-26 09:09:01 +0530

    Yet to Complete Commit

---
+++ test.py
@@ -0,0 +1,94 @@
+from pymongo import MongoClient
+import random
+
+# client = MongoClient('localhost', 27017)
+# db = client['tim_gen']
+
+# l = set()
+# with open('data.txt', 'r') as f:
+#     t = f.readlines()
+#     for _ in t:
+#         tt = _.split('/')
+#         for _ in tt:
+#             ttt = _.rstrip('\n')
+#             ttt = ttt.strip()
+#             l.add(ttt)
+#     l.remove("")
+# print(l,len(l))
+time = [1, 2, 3, 4, 5, 6]
+
+print(random.choice(time,weights=[0.19,0.19,0.19,0.19,0.19,0.05],k = 10))
+
+# Dr. C.Kiran Mai
+# Dr.G.Ramesh chandra
+# Dr.N.Sandhya
+# Dr. P.Neelakantan
+# Dr.M. Rajasekhar
+# Dr.B.V.Kiranmayee
+# V.Baby
+# Dr.S.Nagini
+# M. Gangappa
+# Dr.P.V.Siva Kumar
+# Dr.T.Sunil Kumar
+# Dr.Y.Sagar
+# Dr.A.Brahmananda Reddy
+# Dr. Deepak Sukheja
+# Dr.P.Subhash
+# A.Madhavi
+# N.V. Sailaja
+# A.Kousar Nikhath
+# D.N. Vasundhara
+# R. Vasavi
+# R. Vijaya Saraswathi
+# G.S. Ramesh
+# P. Radhika
+# N. Sravani
+# T.Gnana Prakash
+# G.Nagaraju
+# R.Kranthi Kumar
+# P.Venkateswara Rao
+# Y.Bhanu Sree
+# N. Sandeep Chaitanya
+# S. Jahnavi
+# M.Ravi Kanth
+# P Bharath kumar Chowdary
+# Priya Bhatnagar
+# P Rama Krishna
+# Priyanka Singh
+# N Lakshmi Kalyani
+# S.Kranthi Kumar
+# K.Bheema Lingappa
+# L.Indira
+# Dr.K.Srinivas
+# S.Swapna
+# Dr.Ch.Suresh
+# Krithi Ohri
+# K. Jhansi Lakshmi Bai
+# M.Venkata Krishna Rao
+# S.Nyemeesha
+# V.Harish
+# Shaik Arshiya Julma
+# A.Chennabasamma
+# P.Jyothi
+# M.Sangeetha
+# A.Manusha Reddy
+# Lakshmi Prasudha
+
+# 18HM1MG01
+# Engineering Economics and Accountancy
+# 18PC1CS03
+# Software Engineering
+# 18PC1IT03
+# Java Programming
+# 18PC1IT04
+# Formal Languages and Automata Theory
+# 18PC1CS04
+# Design and Analysis of Algorithms
+# 18PC1CS05
+# Database Management Systems
+# 18PC2IT02
+# Java Programming Laboratory
+# 18PC2CS02
+# Database Management Systems Laboratory
+# 18PC2IT03
+# IT Workshop
